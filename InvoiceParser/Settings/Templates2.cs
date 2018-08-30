d85c20d39474c50f214341f07f8f8180bbec0ca1
cliversoft <sergey.stoyan@gmail.com>
2018-08-31 00:34:40 +0300

    .

--- InvoiceParser/Settings/Templates2.cs
+++ InvoiceParser/Settings/Templates2.cs
@@ -1,84 +1,118 @@
-//********************************************************************************************
-//Author: Sergey Stoyan
-//        sergey.stoyan@gmail.com
-//        http://www.cliversoft.com
-//********************************************************************************************
-using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
-using System.Data.Linq;
-using System.Linq;
-using System.Drawing;
-using System.Collections.Specialized;
-using Cliver.PdfDocumentParser;
-using System.Net;
+////********************************************************************************************
+////Author: Sergey Stoyan
+////        sergey.stoyan@gmail.com
+////        http://www.cliversoft.com
+////********************************************************************************************
+//using System;
+//using System.Collections.Generic;
+//using System.Text.RegularExpressions;
+//using System.Data.Linq;
+//using System.Linq;
+//using System.Drawing;
+//using System.Collections.Specialized;
+//using Cliver.PdfDocumentParser;
+//using System.Net;
 
-namespace Cliver.InvoiceParser
-{
-    public partial class Settings
-    {
-        [Cliver.Settings.Obligatory]
-        public static readonly Templates2Settings Templates2;
+//namespace Cliver.InvoiceParser
+//{
+//    public partial class Settings
+//    {
+//        [Cliver.Settings.Obligatory]
+//        public static readonly TemplatesSettings Templates;
 
-        public class Template2
-        {
-            public bool Active = true;
-            public Cliver.PdfDocumentParser.Template BaseTemplate;
-            public string Group;
-        }
+//        public class TemplatesSettings : Cliver.Settings
+//        {
+//            public Template InitialTemplate;
+//            public List<Template> Templates = new List<Template>();//preserving order of matching: only the first match is to be applied
 
-        public class Templates2Settings : Cliver.Settings
-        {
-            public Template2 InitialTemplate;
-            public List<Template2> Templates = new List<Template2>();//preserving order of matching: only the first match is to be applied
+//            public override void Loaded()
+//            {
+//                //if (Templates.Count < 1)
+//                //    Templates.Add(CreateInitialTemplate());
 
-            public override void Loaded()
-            {
-                //if (Templates.Count < 1)
-                //    Templates.Add(CreateInitialTemplate());
-            }
+//                if (Templates.Count > 0 && Templates[0].BaseTemplate == null)//old format conversion
+//                {
+//                    FileSystemRoutines.CopyFile(__File, Config.StorageDir + "\\_Templates.Cliver.InvoiceParser.Settings+_TemplatesSettings.json", true);
+//                    Settings._Templates.Reload();
+//                    Templates.Clear();
+//                    foreach (_Template _t in Settings._Templates.Templates)
+//                    {
+//                        Template t = new Template
+//                        {
+//                            Active = _t.Active,
+//                            Group = _t.Group,
+//                            BaseTemplate = new PdfDocumentParser.Template
+//                            {
+//                                AutoDeskew = _t.AutoDeskew,
+//                                AutoDeskewThreshold = _t.AutoDeskewThreshold,
+//                                BrightnessTolerance = _t.BrightnessTolerance,
+//                                DifferentPixelNumberTolerance = _t.DifferentPixelNumberTolerance,
+//                                DocumentFirstPageRecognitionMarks = _t.DocumentFirstPageRecognitionMarks,
+//                                Fields = _t.Fields,
+//                                FileFilterRegex = _t.FileFilterRegex,
+//                                FindBestImageMatch = _t.FindBestImageMatch,
+//                                FloatingAnchors = _t.FloatingAnchors,
+//                                Name = _t.Name,
+//                                PagesRotation = _t.PagesRotation,
+//                                TestFile = _t.TestFile,
+//                                TestPictureScale = _t.TestPictureScale,
+//                            }
+//                        };
+//                        Templates.Add(t);
+//                    }
+//                    Save();
+//                    Reload();
+//                }
+//            }
 
-            public override void Saving()
-            {
-            }
+//            public override void Saving()
+//            {
+//            }
 
-            public override void Saved()
-            {
-                Settings.SynchronizationSettings.SynchronizeUploadFile(this.__File);
-            }
+//            public override void Saved()
+//            {
+//                Settings.SynchronizationSettings.SynchronizeUploadFile(this.__File);
+//            }
 
-            public Template2 CreateInitialTemplate()
-            {
-                if (InitialTemplate != null)
-                {
-                    string ts = SerializationRoutines.Json.Serialize(InitialTemplate);
-                    return SerializationRoutines.Json.Deserialize<Template2>(ts);
-                }
-                return new Template2
-                {
-                    Active = true,
-                    BaseTemplate = new PdfDocumentParser.Template
-                    {
-                        AutoDeskew = false,
-                        BrightnessTolerance = 0.4f,
-                        DifferentPixelNumberTolerance = 0.01f,
-                        Fields = new List<Template.Field> {
-                        new Template.Field { Name = "INVOICE#" , Rectangle=new Template.RectangleF(0,0,10,10)},
-                        new Template.Field { Name = "JOB#", Rectangle=new Template.RectangleF(0,0,10,10) },
-                        new Template.Field { Name = "PO#", Rectangle=new Template.RectangleF(0,0,10,10) },
-                        new Template.Field { Name = "COST" , Rectangle=new Template.RectangleF(0,0,10,10)},
-                    },
-                        Name = "-new-",
-                        FileFilterRegex = new Regex(@"\.pdf$", RegexOptions.IgnoreCase),
-                        FindBestImageMatch = false,
-                        FloatingAnchors = new List<Template.FloatingAnchor>(),
-                        DocumentFirstPageRecognitionMarks = new List<Template.Mark>(),
-                        PagesRotation = Template.PageRotations.NONE,
-                        TestPictureScale = 1.3m,
-                        TestFile = "",
-                    }
-                };
-            }
-        }
-    }
-}
+//            public Template CreateInitialTemplate()
+//            {
+//                if (InitialTemplate != null)
+//                {
+//                    string ts = SerializationRoutines.Json.Serialize(InitialTemplate);
+//                    return SerializationRoutines.Json.Deserialize<Template>(ts);
+//                }
+//                return new Template
+//                {
+//                    Active = true,
+//                    BaseTemplate = new PdfDocumentParser.Template
+//                    {
+//                        AutoDeskew = false,
+//                        BrightnessTolerance = 0.4f,
+//                        DifferentPixelNumberTolerance = 0.01f,
+//                        Fields = new List<PdfDocumentParser.Template.Field> {
+//                        new PdfDocumentParser.Template.Field { Name = "INVOICE#" , Rectangle=new PdfDocumentParser.Template.RectangleF(0,0,10,10)},
+//                        new PdfDocumentParser.Template.Field { Name = "JOB#", Rectangle=new PdfDocumentParser.Template.RectangleF(0,0,10,10) },
+//                        new PdfDocumentParser.Template.Field { Name = "PO#", Rectangle=new PdfDocumentParser.Template.RectangleF(0,0,10,10) },
+//                        new PdfDocumentParser.Template.Field { Name = "COST" , Rectangle=new PdfDocumentParser.Template.RectangleF(0,0,10,10)},
+//                    },
+//                        Name = "-new-",
+//                        FileFilterRegex = new Regex(@"\.pdf$", RegexOptions.IgnoreCase),
+//                        FindBestImageMatch = false,
+//                        FloatingAnchors = new List<PdfDocumentParser.Template.FloatingAnchor>(),
+//                        DocumentFirstPageRecognitionMarks = new List<PdfDocumentParser.Template.Mark>(),
+//                        PagesRotation = PdfDocumentParser.Template.PageRotations.NONE,
+//                        TestPictureScale = 1.3m,
+//                        TestFile = "",
+//                    }
+//                };
+//            }
+//        }
+//    }
+
+//    public class Template
+//    {
+//        public bool Active = true;
+//        public Cliver.PdfDocumentParser.Template BaseTemplate;
+//        public string Group;
+//    }
+//}
