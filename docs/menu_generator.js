f71a41077b66984c5ba347caf2f64b11f3aaf543
cliversoft <sergey.stoyan@gmail.com>
2018-11-11 16:36:05 +0200

    .

--- docs/menu_generator.js
+++ docs/menu_generator.js
@@ -13,7 +13,7 @@ Html body must have:
  Every <h1>, <h2>... tag becomes an item.
  
  USAGE:
- This script must be embedded in the end of html.
+ This script must be embedded in the end of html body.
  Also menu_generator.css must be linked.
 */
 var convert = function(mode){
