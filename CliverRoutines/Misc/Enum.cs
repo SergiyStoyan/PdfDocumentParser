def6792e5b1114355cc40424424401388f65581e
Sergey Stoyan <sergey.stoyan@gmail.com>
2019-09-04 17:59:08 +0300

    Update Enum.cs

--- CliverRoutines/Misc/Enum.cs
+++ CliverRoutines/Misc/Enum.cs
@@ -17,27 +17,36 @@ namespace Cliver
     {
         public static readonly NullableEnum<T> NULL = new NullableEnum<T>(null);
 
-        new public virtual string ToString()
+        public override string ToString()
         {
             return Value.ToString();
         }
 
         protected NullableEnum(T value)
         {
-            this.Value = value;
+            Value = value;
+        }
+
+        public NullableEnum()
+        {
         }
 
         public T Value { get; private set; }
 
         public Dictionary<string, T> ToDictionary()
         {
-            return this.GetType().GetFields(BindingFlags.Public | BindingFlags.Static).ToDictionary(x => x.Name, x => ((Enum<T>)x.GetValue(this)).Value);
+            return GetType().GetFields(BindingFlags.Public | BindingFlags.Static).Where(x => x.FieldType.IsSubclassOf(typeof(NullableEnum<T>))).ToDictionary(x => x.Name, x => ((NullableEnum<T>)x.GetValue(this)).Value);
+        }
+
+        public List<T> ToList()
+        {
+            return GetType().GetFields(BindingFlags.Public | BindingFlags.Static).Where(x => x.FieldType.IsSubclassOf(typeof(NullableEnum<T>))).Select(x => ((NullableEnum<T>)x.GetValue(this)).Value).ToList();
         }
     }
 
     public class Enum<T>
     {
-        new public virtual string ToString()
+        public override string ToString()
         {
             return Value.ToString();
         }
@@ -55,13 +64,35 @@ namespace Cliver
 
         public Dictionary<string, T> ToDictionary()
         {
-            return GetType().GetFields(BindingFlags.Public | BindingFlags.Static).ToDictionary(x => x.Name, x => ((Enum<T>)x.GetValue(this)).Value);
+            return GetType().GetFields(BindingFlags.Public | BindingFlags.Static).Where(x => x.FieldType.IsSubclassOf(typeof(Enum<T>))).ToDictionary(x => x.Name, x => ((Enum<T>)x.GetValue(this)).Value);
         }
 
         public List<T> ToList()
         {
-            return GetType().GetFields(BindingFlags.Public | BindingFlags.Static).Select(x => ((Enum<T>)x.GetValue(this)).Value).ToList();
+            return GetType().GetFields(BindingFlags.Public | BindingFlags.Static).Where(x => x.FieldType.IsSubclassOf(typeof(Enum<T>))).Select(x => ((Enum<T>)x.GetValue(this)).Value).ToList();
+        }
+    }
+
+    internal class EnumExample : Cliver.Enum<string>
+    {
+        public static readonly EnumExample EMPTY = new EnumExample("");
+        public static readonly EnumExample VALUE1 = new EnumExample("VALUE1");
+        public static readonly EnumExample VALUE2 = new EnumExample("VALUE2");
+        public static readonly EnumExample VALUE3 = new EnumExample("VALUE3");
+
+        internal EnumExample(string value) : base(value) { }
+    }
+
+    public static class StringValues
+    {
+        public static Dictionary<string, string> ToDictionary()
+        {
+            return typeof(StringValues).GetFields(BindingFlags.Public | BindingFlags.Static).Where(x => x.FieldType == typeof(string)).ToDictionary(x => x.Name, x => (string)x.GetValue(null));
+        }
+
+        public static List<string> ToList()
+        {
+            return typeof(StringValues).GetFields(BindingFlags.Public | BindingFlags.Static).Where(x => x.FieldType == typeof(string)).Select(x => (string)x.GetValue(null)).ToList();
         }
     }
 }
-
