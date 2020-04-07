6651f213120db0114da5977fc1e53d730839ac5c
Sergey Stoyan <sergey.stoyan@gmail.com>
2020-04-07 10:55:34 +0300

    Update Page.deskew.cs

--- Page.deskew.cs
+++ Page.deskew.cs
@@ -83,7 +83,7 @@
 //            {
 //                var startIndex = shift < 0 ? -shift : 0;
 //                var endIndex = shift > 0 ? height - shift - 1 : height - 1;
-                
+
 //                var res = 0;
 //                for (int i = startIndex; i <= endIndex; i++)
 //                    res += -Math.Abs(strip1[i] - strip2[i + shift]);
@@ -98,5 +98,19 @@
 
 //            return bestShift;
 //        }
+
+//public static Bitmap Rotate(Bitmap img, double angle)
+//{
+//    var rotated = new Bitmap(img.Width, img.Height);
+//    using (var gr = Graphics.FromImage(rotated))
+//    {
+//        gr.InterpolationMode = InterpolationMode.HighQualityBicubic;
+//        gr.TranslateTransform(img.Width / 2, img.Height / 2);
+//        gr.RotateTransform(-(float)angle);
+//        gr.DrawImage(img, -img.Width / 2, -img.Height / 2, img.Width, img.Height);
+//    }
+
+//    return rotated;
+//}
 //    }
 //}
