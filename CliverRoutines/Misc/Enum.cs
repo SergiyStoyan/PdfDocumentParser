816620b0cb988f50e711e3645048e95fe0b9e841
Sergey Stoyan <sergey.stoyan@gmail.com>
2020-06-19 02:45:44 +0300

    .

--- CliverRoutines/Misc/Enum.cs
+++ CliverRoutines/Misc/Enum.cs
@@ -14,141 +14,76 @@ using System.Reflection;
 
 namespace Cliver
 {
-    public class Enum<T>
+    public class Enum<V>
     {
         public override string ToString()
         {
             return Value.ToString();
         }
 
-        protected Enum(T value)
+        protected Enum(V value)
         {
             Value = value;
         }
 
-        public T Value { get; protected set; }
+        public V Value { get; protected set; }
 
-        static public Dictionary<string, E> ToDictionary<E>() where E : Enum<T>
+        static public Dictionary<string, E> ToDictionary<E>() where E : Enum<V>
         {
-            return typeof(E).GetFields(BindingFlags.Public | BindingFlags.Static).Where(x => x.FieldType.IsSubclassOf(typeof(Enum<T>))).ToDictionary(x => x.Name, x => ((E)x.GetValue(null)));
+            return typeof(E).GetFields(BindingFlags.Public | BindingFlags.Static).Where(x => x.FieldType.IsSubclassOf(typeof(Enum<V>))).ToDictionary(x => x.Name, x => ((E)x.GetValue(null)));
         }
 
-        static public List<E> ToList<E>() where E : Enum<T>
+        static public List<E> ToList<E>() where E : Enum<V>
         {
-            return typeof(E).GetFields(BindingFlags.Public | BindingFlags.Static).Where(x => x.FieldType.IsSubclassOf(typeof(Enum<T>))).Select(x => ((E)x.GetValue(null))).ToList();
+            return typeof(E).GetFields(BindingFlags.Public | BindingFlags.Static).Where(x => x.FieldType.IsSubclassOf(typeof(Enum<V>))).Select(x => ((E)x.GetValue(null))).ToList();
         }
 
-        static public E Parse<E>(string value, bool ignoreCase = false) where E : Enum<T>
+        static public E Parse<E>(string valueStr, bool ignoreCase = false) where E : Enum<V>
         {
-            value = value.Trim();
-            foreach (E e in ToList<E>())
-            {
-                if (0 == string.Compare(e.Value.ToString(), value, ignoreCase))
-                    return e;
-            }
-            return null;
-        }
-
-        public bool IsAmong<E>(params E[] values) where E : Enum<T>
-        {
-            return values.FirstOrDefault(a => a == (E)this) != null;
-        }
-    }
-
-    public class NullableEnum<T> : Enum<T> where T : class
-    {
-        //Example! You have to define this field in your class
-        //public static readonly NullableEnum<T> NULL = new NullableEnum<T>(null);
-
-        protected NullableEnum(T value) : base(value)
-        {
-        }
-
-        new static public E Parse<E>(string value, bool ignoreCase = false) where E : Enum<T>
-        {
-            if (value != null)
-                value = value.Trim();
+            if (valueStr != null)
+                valueStr = valueStr.Trim();
             foreach (E e in ToList<E>())
             {
                 if (e.Value == null)
-                    if (value == null)
+                    if (valueStr == null)
                         return e;
                     else
                         continue;
-                if (value == null)
+                if (valueStr == null)
                     continue;
-                if (0 == string.Compare(e.Value.ToString(), value, ignoreCase))
+                if (0 == string.Compare(e.Value.ToString(), valueStr, ignoreCase))
                     return e;
             }
             return null;
+        }
+
+        public bool IsAmong<E>(params E[] objects) where E : Enum<V>
+        {
+            return objects.FirstOrDefault(a => a == (E)this) != null;
         }
     }
 
     internal class EnumExample : Cliver.Enum<string>
     {
-        public static readonly EnumExample EMPTY = new EnumExample("");
+        public static readonly EnumExample NULL = new EnumExample(null);
+        public static readonly EnumExample EMPTY = new EnumExample(string.Empty);
         public static readonly EnumExample VALUE1 = new EnumExample("VALUE1");
         public static readonly EnumExample VALUE2 = new EnumExample("VALUE2");
         public static readonly EnumExample VALUE3 = new EnumExample("VALUE3");
 
-        internal EnumExample(string value) : base(value) { }
+        EnumExample(string value) : base(value) { }
     }
 
-    /// <summary>
-    /// TBD!!!
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class Set<T>
-    {
-        public override string ToString()
-        {
-            return string.Join(",", Values.Select(a => a.ToString()));
-        }
+    //public static class StringValues
+    //{
+    //    public static Dictionary<string, string> ToDictionary()
+    //    {
+    //        return typeof(StringValues).GetFields(BindingFlags.Public | BindingFlags.Static).Where(x => x.FieldType == typeof(string)).ToDictionary(x => x.Name, x => (string)x.GetValue(null));
+    //    }
 
-        protected Set(IEnumerable<T> values)
-        {
-            Values = values.ToList();
-        }
-
-        public List<T> Values { get; protected set; }
-
-        static public Dictionary<string, S> ToDictionary<S>() where S : Set<T>
-        {
-            return typeof(S).GetFields(BindingFlags.Public | BindingFlags.Static).Where(x => x.FieldType.IsSubclassOf(typeof(Set<T>))).ToDictionary(x => x.Name, x => ((S)x.GetValue(null)));
-        }
-
-        static public List<S> ToList<S>() where S : Set<T>
-        {
-            return typeof(S).GetFields(BindingFlags.Public | BindingFlags.Static).Where(x => x.FieldType.IsSubclassOf(typeof(Set<T>))).Select(x => ((S)x.GetValue(null))).ToList();
-        }
-
-        //static public S Parse<S>(string values, string separator=",", bool ignoreCase = false) where S : Set<T>
-        //{
-        //    value = value.Trim();
-        //    foreach (T v in ToList<E>())
-        //    {
-        //        if (0 == string.Compare(v.ToString(), value, ignoreCase))
-        //            return (E)Activator.CreateInstance(typeof(E), v);
-        //    }
-        //    return null;
-        //}
-
-        //public bool ContainsAll(IEnumerable<T> values)
-        //{
-
-        //}
-    }
-
-    public static class StringValues
-    {
-        public static Dictionary<string, string> ToDictionary()
-        {
-            return typeof(StringValues).GetFields(BindingFlags.Public | BindingFlags.Static).Where(x => x.FieldType == typeof(string)).ToDictionary(x => x.Name, x => (string)x.GetValue(null));
-        }
-
-        public static List<string> ToList()
-        {
-            return typeof(StringValues).GetFields(BindingFlags.Public | BindingFlags.Static).Where(x => x.FieldType == typeof(string)).Select(x => (string)x.GetValue(null)).ToList();
-        }
-    }
+    //    public static List<string> ToList()
+    //    {
+    //        return typeof(StringValues).GetFields(BindingFlags.Public | BindingFlags.Static).Where(x => x.FieldType == typeof(string)).Select(x => (string)x.GetValue(null)).ToList();
+    //    }
+    //}
 }
---
+++ CliverRoutines/Misc/Set.cs
@@ -0,0 +1,129 @@
+﻿//********************************************************************************************
+//Author: Sergey Stoyan
+//        sergey.stoyan@gmail.com
+//        sergey_stoyan@yahoo.com
+//        http://www.cliversoft.com
+//        26 September 2006
+//Copyright: (C) 2006, Sergey Stoyan
+//********************************************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+
+namespace Cliver
+{
+    public class Set<V, T> where V : Enum<T>
+    {
+        public override string ToString()
+        {
+            return string.Join(",", Values.Select(a => a.ToString()));
+        }
+
+        protected Set(IEnumerable<V> values)
+        {
+            if (values == null)
+                Values = new HashSet<V>();
+            else
+                Values = new HashSet<V>(values);
+        }
+
+        public HashSet<V> Values { get; protected set; }
+
+        public S Append<S>(S set) where S : Set<V, T>
+        {
+            return Append<S>(set.Values.ToArray());
+        }
+
+        public S Append<S>(params V[] values) where S : Set<V, T>
+        {
+            foreach (V v in values)
+                Values.Add(v);
+            return (S)this;
+        }
+
+        public S Remove<S>(S set) where S : Set<V, T>
+        {
+            return Remove<S>(set.Values.ToArray());
+        }
+
+        public S Remove<S>(params V[] values) where S : Set<V, T>
+        {
+            foreach (V v in values)
+                Values.Remove(v);
+            return (S)this;
+        }
+
+        public static bool operator ==(Set<V, T> a, Set<V, T> b)
+        {
+            if (a.Values.Count != b.Values.Count)
+                return false;
+            foreach (V v in b.Values)
+                if (!a.Values.Contains(v))
+                    return false;
+            return true;
+        }
+
+        public static bool operator !=(Set<V, T> a, Set<V, T> b)
+        {
+            return !(a == b);
+        }
+
+        static public S Parse<S>(string valuesStr, string separator = ",", bool ignoreCase = false) where S : Set<V, T>
+        {
+            if (valuesStr == null)
+                valuesStr = string.Empty;
+            HashSet<V> allValues = new HashSet<V>((List<V>)typeof(V).GetMethod("ToList").Invoke(null, null));
+            HashSet<V> foundValues = new HashSet<V>();
+            string[] vss = System.Text.RegularExpressions.Regex.Split(valuesStr, @"\s*" + separator + @"\s*");
+            foreach (string vs in vss)
+            {
+                V v = allValues.FirstOrDefault(a => 0 == string.Compare(vs, a.ToString(), ignoreCase));
+                if (v == null)
+                    return null;
+                foundValues.Add(v);
+            }
+            S s = (S)Activator.CreateInstance(typeof(S));
+            s.Values = foundValues;
+            return s;
+        }
+
+        public bool Contains<S>(params V[] values) where S : Set<V, T>
+        {
+            return AcontainsB<S>(Values, values);
+        }
+
+        public bool Contained<S>(params V[] values) where S : Set<V, T>
+        {
+            return AcontainsB<S>(values, Values);
+        }
+
+        static bool AcontainsB<S>(IEnumerable<V> aValues, IEnumerable<V> bValues) where S : Set<V, T>
+        {
+            if (bValues == null)
+                return true;
+            if (aValues == null)
+                return false;
+            foreach (V v in bValues)
+                if (!aValues.Contains(v))
+                    return false;
+            return true;
+        }
+    }
+
+    internal class SetExample : Cliver.Set<EnumExample, string>
+    {
+        internal SetExample(IEnumerable<EnumExample> values) : base(values) { }
+        internal SetExample(params EnumExample[] values) : base(values) { }
+
+        static void test()
+        {
+            SetExample empty = new SetExample(EnumExample.EMPTY);
+            SetExample v1 = new SetExample(EnumExample.VALUE1);
+            SetExample v2 = new SetExample(EnumExample.VALUE2, EnumExample.VALUE3);
+            SetExample v123 = new SetExample(EnumExample.VALUE1, EnumExample.VALUE2, EnumExample.VALUE3);
+            SetExample se = empty.Append(v123).Remove<SetExample>(EnumExample.VALUE3);
+        }
+    }
+}
