eb74cc94a49a57852e445aab390a9a94f7082f30
Sergey Stoyan <sergey.stoyan@gmail.com>
2021-03-29 21:31:18 +0300

    .

--- Ocr.cs
+++ Ocr.cs
@@ -345,7 +345,7 @@ namespace Cliver.PdfDocumentParser
                         if (spaceAutoInsert && /*cb.Char != " " &&*/ lines[i].CharBoxs.Count > 0)
                         {
                             CharBox cb0 = lines[i].CharBoxs[lines[i].CharBoxs.Count - 1];
-                            if (/*cb0.Char != " " && */cb.R.Left - cb0.R.Right > (cb0.R.Width / cb0.R.Height + cb.R.Width / cb.R.Height) * textAutoInsertSpace.Threshold)
+                            if (/*cb0.Char != " " && */cb.R.Left - cb0.R.Right > (/*cb0.R.Width*/0.8 / cb0.R.Height + /*cb.R.Width*/0.8 / cb.R.Height) * textAutoInsertSpace.Threshold)
                             {
                                 float spaceWidth = (cb0.R.Width + cb.R.Width) / 2;
                                 int spaceNumber = (int)Math.Ceiling((cb.R.Left - cb0.R.Right) / spaceWidth);
