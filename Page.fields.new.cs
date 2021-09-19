e697870f399737cf0161dda6fa9b2e125b97efc1
Sergey Stoyan <sergey.stoyan@gmail.com>
2021-09-19 21:51:55 +0300

    Update Page.fields.new.cs

--- Page.fields.new.cs
+++ Page.fields.new.cs
@@ -49,7 +49,7 @@ namespace Cliver.PdfDocumentParser
                     type = field.Type;
                 this.type = type.Value;
             }
-            Page page;
+           readonly Page page;
             internal readonly Template.Field Field;
             Template.Field.Types type;
 
@@ -57,9 +57,243 @@ namespace Cliver.PdfDocumentParser
             {
                 foreach (RectangleF ar in page.getFieldMatchRectangles(Field))
                 {
-                    throw new Exception("TBD");
-                    yield return null;
+                    yield return getValue(ar);
                 }
+            }
+
+            object getValue(RectangleF ar)
+            {
+                if (ar == null || TableFieldActualInfo?.Found == false)
+                    return null;
+                switch (type)
+                {
+                    case Template.Field.Types.PdfText:
+                        {
+                            List<string> ss = getPdfTextLines(ar);
+                            return ss == null ? null : string.Join("\r\n", ss);
+                        }
+                    case Template.Field.Types.PdfTextLines:
+                        return getPdfTextLines(ar);
+                    case Template.Field.Types.PdfCharBoxs:
+                        return getPdfCharBoxs(ar);
+                    case Template.Field.Types.OcrText:
+                        {
+                            List<string> ss = getOcrTextLines(ar);
+                            return ss == null ? null : string.Join("\r\n", ss);
+                        }
+                    case Template.Field.Types.OcrTextLines:
+                        return getOcrTextLines(ar);
+                    case Template.Field.Types.OcrCharBoxs:
+                        return getOcrCharBoxs(ar);
+                    case Template.Field.Types.Image:
+                        return getImage(ar);
+                    case Template.Field.Types.OcrTextLineImages:
+                        return getOcrTextLineImages(ar);
+                    default:
+                        throw new Exception("Unknown option: " + type);
+                }
+            }
+
+            List<string> getPdfTextLines(RectangleF ar)
+            {
+                TextAutoInsertSpace textAutoInsertSpace = Field.TextAutoInsertSpace != null ? Field.TextAutoInsertSpace : page.PageCollection.ActiveTemplate.TextAutoInsertSpace;
+
+                if (Field.ColumnOfTable == null)
+                    return Pdf.GetTextLinesSurroundedByRectangle(page.PdfCharBoxs, ar, textAutoInsertSpace);
+
+                List<string> ls = new List<string>();
+                List<Pdf.CharBox> cbs = (List<Pdf.CharBox>)TableFieldActualInfo.GetValue(Template.Field.Types.PdfCharBoxs);
+                foreach (Line<Pdf.CharBox> l in GetLines(cbs, textAutoInsertSpace, null))
+                {
+                    StringBuilder sb = new StringBuilder();
+                    foreach (Pdf.CharBox cb in l.CharBoxs)
+                        if (cb.R.Left >= ar.Left && cb.R.Right <= ar.Right && cb.R.Top >= ar.Top && cb.R.Bottom <= ar.Bottom)
+                            sb.Append(cb.Char);
+                    ls.Add(sb.ToString());
+                }
+                return ls;
+            }
+
+            List<Pdf.CharBox> getPdfCharBoxs(RectangleF ar)
+            {
+                if (Field.ColumnOfTable == null)
+                    return Pdf.GetCharBoxsSurroundedByRectangle(page.PdfCharBoxs, ar);
+
+                if (!TableFieldActualInfo.Found)
+                    return null;
+                List<Pdf.CharBox> cbs = (List<Pdf.CharBox>)TableFieldActualInfo.GetValue(Template.Field.Types.PdfCharBoxs);
+                return cbs.Where(a => a.R.Left >= ar.Left && a.R.Right <= ar.Right && a.R.Top >= ar.Top && a.R.Bottom <= ar.Bottom).ToList();
+            }
+
+            List<string> getOcrTextLines(RectangleF ar)
+            {
+                Template.Field.Ocr aof = Field as Template.Field.Ocr;
+                TextAutoInsertSpace textAutoInsertSpace = aof?.TextAutoInsertSpace != null ? aof?.TextAutoInsertSpace : page.PageCollection.ActiveTemplate.TextAutoInsertSpace;
+
+                if (Field.ColumnOfTable == null)
+                {
+                    if (aof?.SingleFieldFromFieldImage ?? page.PageCollection.ActiveTemplate.SingleFieldFromFieldImage)
+                    {
+                        List<Ocr.CharBox> cs = Ocr.This.GetCharBoxsSurroundedByRectangle(page.ActiveTemplateBitmap, ar, aof?.TesseractPageSegMode ?? page.PageCollection.ActiveTemplate.TesseractPageSegMode);
+                        if (cs == null)
+                            return null;
+                        return GetTextLines(cs, textAutoInsertSpace, Field.CharFilter ?? page.PageCollection.ActiveTemplate.CharFilter);
+                    }
+                    else
+                        return Ocr.GetTextLinesSurroundedByRectangle(page.ActiveTemplateOcrCharBoxs, ar, textAutoInsertSpace, Field.CharFilter ?? page.PageCollection.ActiveTemplate.CharFilter);
+                }
+
+                if (!TableFieldActualInfo.Found)
+                    return null;
+                List<Ocr.CharBox> cbs = (List<Ocr.CharBox>)TableFieldActualInfo.GetValue(Template.Field.Types.OcrCharBoxs);
+                List<string> ls = new List<string>();
+                if (aof?.ColumnCellFromCellImage ?? page.PageCollection.ActiveTemplate.ColumnCellFromCellImage)
+                {
+                    List<Line<Ocr.CharBox>> ols = GetLines(cbs, null, Field.CharFilter ?? page.PageCollection.ActiveTemplate.CharFilter);
+                    if (aof?.AdjustLineBorders ?? page.PageCollection.ActiveTemplate.AdjustLineBorders)
+                        AdjustBorders(ols, TableFieldActualInfo.actualRectangle.Value);
+                    else
+                        PadLines(ols, Field.LinePaddingY ?? page.PageCollection.ActiveTemplate.LinePaddingY);
+                    foreach (Line<Ocr.CharBox> l in ols)
+                    {
+                        float x = ar.X > TableFieldActualInfo.actualRectangle.Value.X ? ar.X : TableFieldActualInfo.actualRectangle.Value.X;
+                        RectangleF r = new RectangleF(
+                            x,
+                            l.Top,
+                           (ar.Right < TableFieldActualInfo.actualRectangle.Value.Right ? ar.Right : TableFieldActualInfo.actualRectangle.Value.Right) - x,
+                           l.Bottom - l.Top
+                            );
+                        List<Ocr.CharBox> cs = Ocr.This.GetCharBoxsSurroundedByRectangle(page.ActiveTemplateBitmap, r, aof?.TesseractPageSegMode ?? page.PageCollection.ActiveTemplate.TesseractPageSegMode);
+                        ls.Add(cs != null ? string.Join("", GetTextLines(cs, textAutoInsertSpace, Field.CharFilter ?? page.PageCollection.ActiveTemplate.CharFilter)) : "");
+                    }
+                }
+                else
+                {
+                    foreach (Line<Ocr.CharBox> l in GetLines(cbs, textAutoInsertSpace, Field.CharFilter ?? page.PageCollection.ActiveTemplate.CharFilter))
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        foreach (Ocr.CharBox cb in l.CharBoxs)
+                            if (cb.R.Left >= ar.Left && cb.R.Right <= ar.Right && cb.R.Top >= ar.Top && cb.R.Bottom <= ar.Bottom)
+                                sb.Append(cb.Char);
+                        ls.Add(sb.ToString());
+                    }
+                }
+                return ls;
+            }
+
+            List<Ocr.CharBox> getOcrCharBoxs(RectangleF ar)
+            {
+                Template.Field.Ocr aof = Field as Template.Field.Ocr;
+
+                if (Field.ColumnOfTable == null)
+                {
+                    if (aof?.SingleFieldFromFieldImage ?? page.PageCollection.ActiveTemplate.SingleFieldFromFieldImage)
+                        return Ocr.This.GetCharBoxsSurroundedByRectangle(page.ActiveTemplateBitmap, ar, aof?.TesseractPageSegMode ?? page.PageCollection.ActiveTemplate.TesseractPageSegMode);
+                    else
+                        return Ocr.GetCharBoxsSurroundedByRectangle(page.ActiveTemplateOcrCharBoxs, ar);
+                }
+
+                if (!TableFieldActualInfo.Found)
+                    return null;
+                if (aof?.ColumnCellFromCellImage ?? page.PageCollection.ActiveTemplate.ColumnCellFromCellImage)
+                {
+                    float x = ar.X > TableFieldActualInfo.actualRectangle.Value.X ? ar.X : TableFieldActualInfo.actualRectangle.Value.X;
+                    float y = ar.Top > TableFieldActualInfo.actualRectangle.Value.Top ? ar.Top : TableFieldActualInfo.actualRectangle.Value.Top;
+                    RectangleF r = new RectangleF(
+                        x,
+                        y,
+                       (ar.Right < TableFieldActualInfo.actualRectangle.Value.Right ? ar.Right : TableFieldActualInfo.actualRectangle.Value.Right) - x,
+                       (ar.Bottom < TableFieldActualInfo.actualRectangle.Value.Bottom ? ar.Bottom : TableFieldActualInfo.actualRectangle.Value.Bottom) - y
+                        );
+                    return Ocr.This.GetCharBoxsSurroundedByRectangle(page.ActiveTemplateBitmap, ar, aof?.TesseractPageSegMode ?? page.PageCollection.ActiveTemplate.TesseractPageSegMode);
+                }
+                else
+                {
+                    List<Ocr.CharBox> cbs = (List<Ocr.CharBox>)TableFieldActualInfo.GetValue(Template.Field.Types.OcrCharBoxs);
+                    return cbs.Where(a => /*!check: ar.Contains(a.R)*/ a.R.Left >= ar.Left && a.R.Right <= ar.Right && a.R.Top >= ar.Top && a.R.Bottom <= ar.Bottom).ToList();
+                }
+            }
+
+            Bitmap getImage(RectangleF ar)
+            {
+                RectangleF r;
+
+                if (Field.ColumnOfTable != null)
+                {
+                    if (!TableFieldActualInfo.Found)
+                        return null;
+
+                    float x = ar.X > TableFieldActualInfo.actualRectangle.Value.X ? ar.X : TableFieldActualInfo.actualRectangle.Value.X;
+                    float y = ar.Top > TableFieldActualInfo.actualRectangle.Value.Top ? ar.Top : TableFieldActualInfo.actualRectangle.Value.Top;
+                    r = new RectangleF(
+                        x,
+                        y,
+                       (ar.Right < TableFieldActualInfo.actualRectangle.Value.Right ? ar.Right : TableFieldActualInfo.actualRectangle.Value.Right) - x,
+                       (ar.Bottom < TableFieldActualInfo.actualRectangle.Value.Bottom ? ar.Bottom : TableFieldActualInfo.actualRectangle.Value.Bottom) - y
+                        );
+                }
+                else
+                    r = ar;
+
+                using (Bitmap b = page.GetRectangleFromActiveTemplateBitmap(r.X / Settings.Constants.Pdf2ImageResolutionRatio, r.Y / Settings.Constants.Pdf2ImageResolutionRatio, r.Width / Settings.Constants.Pdf2ImageResolutionRatio, r.Height / Settings.Constants.Pdf2ImageResolutionRatio))
+                {
+                    if (b == null)
+                        return null;
+                    return GetImageScaled2Pdf(b);
+                }
+            }
+
+            List<Bitmap> getOcrTextLineImages(RectangleF ar)
+            {
+                Template.Field.Ocr aof = Field as Template.Field.Ocr;
+
+                List<Line<Ocr.CharBox>> ols;
+                float left, width;
+                if (Field.ColumnOfTable == null)
+                {
+                    if (aof?.SingleFieldFromFieldImage ?? page.PageCollection.ActiveTemplate.SingleFieldFromFieldImage)
+                    {
+                        List<Ocr.CharBox> cs = Ocr.This.GetCharBoxsSurroundedByRectangle(page.ActiveTemplateBitmap, ar, aof?.TesseractPageSegMode ?? page.PageCollection.ActiveTemplate.TesseractPageSegMode);
+                        if (cs == null)
+                            return null;
+                        ols = GetLines(cs, null, Field.CharFilter ?? page.PageCollection.ActiveTemplate.CharFilter);
+                    }
+                    else
+                        ols = Ocr.GetLinesSurroundedByRectangle(page.ActiveTemplateOcrCharBoxs, ar, null, Field.CharFilter ?? page.PageCollection.ActiveTemplate.CharFilter);
+                    if (aof?.AdjustLineBorders ?? page.PageCollection.ActiveTemplate.AdjustLineBorders)
+                        AdjustBorders(ols, ar);
+                    else
+                        PadLines(ols, Field.LinePaddingY ?? page.PageCollection.ActiveTemplate.LinePaddingY);
+                    left = ar.Left;
+                    width = ar.Width;
+                }
+                else
+                {
+                    if (!TableFieldActualInfo.Found)
+                        return null;
+                    List<Ocr.CharBox> cbs = (List<Ocr.CharBox>)TableFieldActualInfo.GetValue(Template.Field.Types.OcrCharBoxs);
+                    if (aof?.ColumnCellFromCellImage ?? page.PageCollection.ActiveTemplate.ColumnCellFromCellImage)
+                        ols = GetLines(cbs, null, Field.CharFilter ?? page.PageCollection.ActiveTemplate.CharFilter);
+                    else
+                        ols = GetLines(cbs, null, Field.CharFilter ?? page.PageCollection.ActiveTemplate.CharFilter);
+                    if (aof?.AdjustLineBorders ?? page.PageCollection.ActiveTemplate.AdjustLineBorders)
+                        AdjustBorders(ols, TableFieldActualInfo.actualRectangle.Value);
+                    else
+                        PadLines(ols, Field.LinePaddingY ?? page.PageCollection.ActiveTemplate.LinePaddingY);
+                    left = ar.X > TableFieldActualInfo.actualRectangle.Value.X ? ar.X : TableFieldActualInfo.actualRectangle.Value.X;
+                    width = (ar.Right < TableFieldActualInfo.actualRectangle.Value.Right ? ar.Right : TableFieldActualInfo.actualRectangle.Value.Right) - left;
+                }
+
+                List<Bitmap> ls = new List<Bitmap>();
+                foreach (Line<Ocr.CharBox> l in ols)
+                {
+                    RectangleF r = new RectangleF(left, l.Top, width, l.Bottom - l.Top);
+                    using (Bitmap b = page.GetRectangleFromActiveTemplateBitmap(r.X / Settings.Constants.Pdf2ImageResolutionRatio, r.Y / Settings.Constants.Pdf2ImageResolutionRatio, r.Width / Settings.Constants.Pdf2ImageResolutionRatio, r.Height / Settings.Constants.Pdf2ImageResolutionRatio))
+                    {
+                        ls.Add(b == null ? b : GetImageScaled2Pdf(b));
+                    }
+                }
+                return ls;
             }
         }
 
